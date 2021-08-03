f3a1d5099001a7a12b71b3765bdf1737a139a2b1
DorivalRamos <dorival.millan@bandtec.com.br>
2021-08-03 12:05:56 -0300

    Adicionando a criação do banco em linhas de codigo em app.py

--- app.py
+++ app.py
@@ -1,57 +1,50 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
+import flask
+from flask_mail import Mail, Message
 from flask_sqlalchemy import SQLAlchemy
-
+import sqlalchemy
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sgzluegb:WFyHBC5SHUHkHzJfnd02fZtoSdwHJnCv@kesavan.db.elephantsql.com/sgzluegb'
+app.secret_key = 'bluedtech'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lvuqxbop:NdMagrrxBtguNA5R0ymG2Igafdj6nWPK@tuffi.db.elephantsql.com/lvuqxbop'
 db = SQLAlchemy(app)
-
-class Projeto(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name_game = db.Column(db.String, nullable=False)
-    game_description = db.Column(db.String, nullable=False)
-    game_grade = db.Column(db.Float, nullable=False)
-    game_parental_rating = db.Column(db.Integer, nullable=False)
-    game_type = db.Column(db.String, nullable=False)
-    game_release = db.Column(db.Integer, nullable=False)
-    game_trailer = db.Column(db.String, nullable=False)
-    #Falta colocar o upload de imagens
-    def __init__(self, name_game, game_description, game_grade, game_parental_rating, game_type, game_release, game_trailer):
-        self.name_game = name_game
-        self.game_description = game_description
-        self.game_grade = game_grade
-        self.game_parental_rating = game_parental_rating
-        self.game_type = game_type
-        self.game_release = game_release
-        self.game_trailer = game_trailer
-
 
 @app.route('/')
 def index():
-    projetos = Projeto.query.all()
-    return render_template('index.html', projetos=projetos)
+    return render_template('index.html')
 
-@app.route('admin-adicionar-titulos')
-def admin_adicionar_titulos():
-    projetos = Projeto.query.all() # Busca todos os projetos no banco e coloca na veriável projetos, que se transforma em uma lista.
-    return render_template('admin-adicionar-titulos.html', projetos=projetos)
+@app.route('/admin-adicionar-titulos.html')
+def adm_titulos():
+    return render_template('admin-adicionar-titulos.html')
 
-@app.route('/new', methods=['POST', 'GET'])
-def new():
-   if request.method == 'POST':
-      projeto = Projeto(
-         request.form['name_game'],
-         request.form['game_description'],
-         request.form['game_grade'],
-         request.form['game_parental_rating'],
-         request.form['game_type'],
-         request.form['game_release'],
-         request.form['game_trailer']
-      )
-      db.session.add(projeto) #sintaxe do sqlalchemy ele prepara o objeto para o banco de dados
-      db.session.commit()
-      return redirect('/adm')
+class info_jogos(db.Model):
+    id = db.Column(db.Integer, primary_key=True,autoincrement=True)
+    nomeJogo = db.Column(db.String(50),nullable=True)
+    imagemLink = db.Column(db.String(500), nullable=False)
+    notaMetric = db.Column(db.Integer,nullable=False)
+    classificacao = db.Column(db.Integer,nullable=False)
+    genero = db.Column(db.String(100),nullable=False)
+    dataLancamento = db.Column(db.Integer,nullable=False)
+    produtora = db.Column(db.String(100),nullable=False)
+    trailler = db.Column(db.String(500),nullable=False)
+    logoLink = db.Column(db.String(500),nullable=False)
 
 
+    def __init__(self, nomeJogo, imagemLink, notaMetric, classificacao, genero, dataLancamento, produtora, trailler, logoLink):
+        self.nome= nomeJogo
+        self.imagemLink = imagemLink
+        self.notaMetric = notaMetric
+        self.classificacao = classificacao
+        self.classificacao = classificacao
+        self.genero = genero
+        self.dataLancamento = dataLancamento
+        self.produtora = produtora
+        self.trailler = trailler
+        self.logoLink = logoLink 
+        
+
+ 
 
 if __name__ == '__main__':
     app.run(debug=True)
+

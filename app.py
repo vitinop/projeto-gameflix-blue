3f3a6104fe10b79ba24b793ae2ff0a547c933398
DorivalRamos <dorival.millan@bandtec.com.br>
2021-08-03 19:51:10 -0300

    Adicionando a a nota /New

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import re
 from flask import Flask, render_template, request, redirect, session, flash
 import flask
 from flask_mail import Mail, Message
@@ -9,13 +10,8 @@ app.secret_key = 'bluedtech'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://lvuqxbop:NdMagrrxBtguNA5R0ymG2Igafdj6nWPK@tuffi.db.elephantsql.com/lvuqxbop'
 db = SQLAlchemy(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/admin-adicionar-titulos.html')
-def adm_titulos():
-    return render_template('admin-adicionar-titulos.html')
+
 
 class info_jogos(db.Model):
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
@@ -35,15 +31,39 @@ class info_jogos(db.Model):
         self.imagemLink = imagemLink
         self.notaMetric = notaMetric
         self.classificacao = classificacao
-        self.classificacao = classificacao
         self.genero = genero
         self.dataLancamento = dataLancamento
         self.produtora = produtora
         self.trailler = trailler
         self.logoLink = logoLink 
         
-
+@app.route('/new', methods = ['GET', 'POST'])
+def new():
+    if request.method == 'POST':
+        jogo = info_jogos(
+            request.form['nomeJogo'],
+            request.form['imagemLink'],
+            request.form['notaMetric'],
+            request.form['classificacao'],
+            request.form['genero'],
+            request.form['dataLancamento'],
+            request.form['produtora'],
+            request.form['trailler'],
+            request.form['logoLink']
+         )    
+        db.session.add(jogo)
+        db.session.commit()
+        return redirect('/')
  
+@app.route('/')
+def index():
+    
+    return render_template('index.html')
+
+
+@app.route('/cadastro-jogos.html')
+def adm_titulos():
+    return render_template('cadastro-jogos.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
